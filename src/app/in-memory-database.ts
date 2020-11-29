6d260b9b31dec7790fb3f19f95f9b80e292382f9
welingtoncassis <welingtoncassis@gmail.com>
2020-11-29 16:33:42 -0300

    feat(mock): add in-memory-web-api para categorias

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,20 @@
+import { InMemoryDbService, RequestInfo } from "angular-in-memory-web-api";
+
+export class InMemoryDatabase implements InMemoryDbService {
+    /**
+     * Retorna todos os objetos que queremos simular na requisição
+     * @param reqInfo 
+     */
+    createDb() {
+        const categories = [
+            { id: 1, name: 'Moradia', description: 'Pagamento de Contas da Casa' },
+            { id: 1, name: 'Saúde', description: 'Plano de Sáude e Remédios' },
+            { id: 1, name: 'Lazer', description: 'Cinema, parques, praia, etc' },
+            { id: 1, name: 'Salário', description: 'Recebimento de Salário' },
+            { id: 1, name: 'Freelas', description: 'Trabalhos como freelancer' }
+        ];
+
+        return { categories }
+    }
+
+}
