c5aabcaa0763490fe5b6dd65426fadd65d130fd1
welingtoncassis <welingtoncassis@gmail.com>
2020-12-02 17:39:29 -0300

    feat(categories): implementando CategoryList

--- src/app/pages/categories/categoty-list/categoty-list.component.ts
+++ src/app/pages/categories/categoty-list/categoty-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../shared/category.service';
+import { Category } from '../shared/category.model';
 
 @Component({
   selector: 'app-categoty-list',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategotyListComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = []
+
+  constructor(
+      private categoryService: CategoryService
+    ) { }
 
   ngOnInit() {
+    this.categoryService.getAll().subscribe(
+      categories => this.categories = categories,
+      error => alert('Erro ao carregar a lista')
+    )
+  }
+
+  deleteCategory(category) {
+    const mustDelete = confirm('Deseja realmente excluir este item?')
+
+    if(mustDelete) {
+      this.categoryService.delete(category.id).subscribe(
+        res => this.categories = this.categories.filter(el => el != category),
+        err => alert('Erro ao excluir a categoria')
+      )
+    }
   }
 
 }
