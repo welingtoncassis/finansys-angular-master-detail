ad3d16a7c507daaab2649b22db8691cbed1427ca
Welington Cruz de Assis <welingtoncassis@gmail.com>
2021-01-16 14:14:05 -0300

    feat(entries): add modulos primng e angular-imask

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -7,6 +7,9 @@ import { EntriesRoutingModule } from './entries-routing.module';
 import { EntryListComponent } from './entry-list/entry-list.component';
 import { EntryFormComponent } from './entry-form/entry-form.component';
 
+import { CalendarModule } from 'primeng/calendar';
+import { IMaskModule } from 'angular-imask';
+
 @NgModule({
   declarations: [
     EntryListComponent,
@@ -15,7 +18,9 @@ import { EntryFormComponent } from './entry-form/entry-form.component';
   imports: [
     CommonModule,
     EntriesRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CalendarModule,
+    IMaskModule
   ]
 })
 export class EntriesModule { }
