18d2511e06b48750c523168ea9afc2149164fe1b
WELINGTON CRUZ DE ASSIS <wcassis@brasilcap.com.br>
2021-03-03 02:43:20 -0300

    refactor(category-list): implementando classe abstrata BaseResourceListComponent

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,36 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../shared/category.service';
 import { Category } from '../shared/category.model';
+import { BaseResourceListComponent } from 'src/app/shared/components/base-resource-list/base-resource-list.component';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
-export class CategoryListComponent implements OnInit {
-
-  categories: Category[] = []
+export class CategoryListComponent extends BaseResourceListComponent<Category> {
 
   constructor(
-      private categoryService: CategoryService
-    ) { }
-
-  ngOnInit() {
-    this.categoryService.getAll().subscribe(
-      categories => this.categories = categories,
-      error => alert('Erro ao carregar a lista')
-    )
-  }
-
-  deleteCategory(category) {
-    const mustDelete = confirm('Deseja realmente excluir este item?')
-
-    if(mustDelete) {
-      this.categoryService.delete(category.id).subscribe(
-        res => this.categories = this.categories.filter(el => el != category),
-        err => alert('Erro ao excluir a categoria')
-      )
-    }
-  }
+      protected categoryService: CategoryService
+    ) {
+      super(categoryService);
+     }
 
 }
