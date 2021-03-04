ce44f991d9270e7e7fbfee27ec4076b7bff3aee6
WELINGTON CRUZ DE ASSIS <wcassis@brasilcap.com.br>
2021-03-04 17:51:04 -0300

    refactor: implementando breadCrumbComponent nos templates

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 
@@ -10,12 +11,14 @@ import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.compon
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule
   ],
   exports:[
     CommonModule,
     ReactiveFormsModule,
-    BreadCrumbComponent
+    BreadCrumbComponent,
+    RouterModule
   ]
 })
 export class SharedModule { }
